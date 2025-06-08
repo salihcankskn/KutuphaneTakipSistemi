aa0723bc85ca91185209df76c26e6296286c1374
Salihcan <125476000+salihcankskn@users.noreply.github.com>
2025-06-08 20:10:23 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBQpkbmWMudB7SGJD5b-Q0uLUor5U-goI0',
-    authDomain: 'kutuphane-takip-projesi-f9dfc.firebaseapp.com',
-    projectId: 'kutuphane-takip-projesi-f9dfc',
-    storageBucket: 'kutuphane-takip-projesi.appspot.com',
-    messagingSenderId: '926225867455',
-    appId: '1:926225867455:web:bf8e6680b174e1f2c49c0a',
+    apiKey: 'fairbaseden  alınan kendi keyin',
+    authDomain: '.......',
+    projectId: '.......',
+    storageBucket: '.........',
+    messagingSenderId: '........',
+    appId: '.............',
   },
 };
-
+// fairbase web uygulaması oluşturma aşamasında gerekli bilgileri verecek oradaki bilgilerle doldurmalısın.
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
